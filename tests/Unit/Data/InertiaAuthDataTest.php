959ba0c615a4f9916c91935ceb800b854ac7e649
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-10-08 22:01:00 +0200

    chore: Use function based test instead of class

--- tests/Unit/Data/InertiaAuthDataTest.php
+++ tests/Unit/Data/InertiaAuthDataTest.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace Tests\Unit\Data;
 
 use App\Data\Inertia\InertiaAuthData;
-use Tests\TestCase;
 
-final class InertiaAuthDataTest extends TestCase
-{
-    public function test_can_instantiate_inertia_auth_data(): void
-    {
-        $data = new InertiaAuthData(null);
-        $this->assertInstanceOf(InertiaAuthData::class, $data);
-    }
-}
+it('can instantiate inertia auth data', function (): void {
+    $data = new InertiaAuthData(
+        user: null,
+    );
+
+    expect($data)->toBeInstanceOf(InertiaAuthData::class);
+});
